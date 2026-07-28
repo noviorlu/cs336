9472884644a2aab83834cfd03ed98f8ba55082c7
Noviorlu <yangchen.jack@outlook.com>
2026-07-27 22:12:03 -0400

    transformer block

--- hw1/cs336_basics/transformer.py
+++ hw1/cs336_basics/transformer.py
@@ -266,16 +266,40 @@ class MultiHeadSelfAttention(nn.Module):
 class TransformerBlock(nn.Module):
     def __init__(
         self, 
+        d_model: int,
+        num_heads: int,
+        d_ff: int,
+        max_seq_len: int | None = None,
+        theta: float | None = None,
+        device: torch.device | None = None, 
+        dtype: torch.dtype | None = None
     ) -> None:
         super().__init__()
+        self.ln1 = RMSNorm(d_model, device=device, dtype=dtype)
+        self.attn = MultiHeadSelfAttention(
+            d_model=d_model, 
+            num_heads=num_heads, 
+            max_seq_len=max_seq_len, 
+            theta=theta, 
+            device=device, 
+            dtype=dtype
+        )
+        self.ln2 = RMSNorm(d_model, device=device, dtype=dtype)
+        self.ffn = SwiGLU(
+            d_model=d_model, 
+            d_ff=d_ff, 
+            device=device, 
+            dtype=dtype
+        )
 
-
-
-
-
-
-
-
-
-
-        
+    def forward(
+        self,
+        x: Float[Tensor, "... seq_len d_model"],
+        mask: Bool[Tensor, "... seq_len seq_len"] | None = None,
+        token_positions: Int[Tensor, "... seq_len"] | None = None
+    ) -> Float[Tensor, "... seq_len d_model"]:
+        # 1. Attention path
+        x = x + self.attn(self.ln1(x), mask=mask, token_positions=token_positions)
+        # 2. Feed-forward path
+        x = x + self.ffn(self.ln2(x))
+        return x
